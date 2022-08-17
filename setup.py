31a877ec1b92aee569a5c292600c42aba47722a5
Tanweer Raza <tanatif@gmail.com>
2022-08-17 21:27:44 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
-#We create setup.py whenever we deploy this project as a library.i.e we wanted to create 
-#library just like sklearn etc
+"""We create setup.py whenever we deploy this project as a library.i.e we wanted to create 
+library just like sklearn etc"""
 
 from setuptools import setup, find_packages
 from typing import List
 
 #Declaring variables for setup functions
-PROJECT_NAME = "housing=prediction"
-VERSION = "0.0.3"
+PROJECT_NAME = "housing-prediction"
+VERSION = "0.0.4"
 AUTHOR = "T Raza"
-DESCRIPTION = "This is my first ML project"
-PACKAGES = find_packages  # This find_packages is going to seach for our own code, all the folders wherever it will find __init__, those folder names it is going to return and then it will try to isnstall.
+DESCRIPTION = "This is my first Machine Learning project"
+
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
-def get_requirements_list()->List[str]:     # ->List[str] This will return list in  which there will be string in it.
-    """
-    Description : This function is going to return list of all the requirements mentioned in requirements.txt file
+HYPHEN_E_DOT = "-e ."
 
+def get_requirements_list()->List[str]:     
+    """
+    # ->List[str] This will return list in  which there will be string in it.
+
+    Description : This function is going to return list of all the requirements mentioned in requirements.txt file
     return - This function is going to return a list which contain name of libraries mentioned in requirements.txt file.
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove("-e .") # .remove("-e .") has been used here because already find_packages has bee used here which does the same work.
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
+    """.remove("-e .") has been used here because already find_packages has bee used here which does the same work."""
 
 
 setup(
@@ -28,6 +36,7 @@ setup(
     version= VERSION,
     author= AUTHOR,
     description= DESCRIPTION,
-    packages= PACKAGES,
+    packages= find_packages(),
     install_requires = get_requirements_list()
 )
+
