bdeb4e021a2ebaa2d8a868d0e7b5767347075da1
Tanweer Raza <tanatif@gmail.com>
2022-08-17 13:32:18 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup
+#We create setup.py whenever we deploy this project as a library.i.e we wanted to create 
+#library just like sklearn etc
+
+from setuptools import setup, find_packages
 from typing import List
 
 #Declaring variables for setup functions
 PROJECT_NAME = "housing=prediction"
-VERSION = "0.0.1"
+VERSION = "0.0.3"
 AUTHOR = "T Raza"
 DESCRIPTION = "This is my first ML project"
-PACKAGES = ["housing"]
+PACKAGES = find_packages  # This find_packages is going to seach for our own code, all the folders wherever it will find __init__, those folder names it is going to return and then it will try to isnstall.
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
 def get_requirements_list()->List[str]:     # ->List[str] This will return list in  which there will be string in it.
@@ -16,7 +19,8 @@ def get_requirements_list()->List[str]:     # ->List[str] This will return list 
     return - This function is going to return a list which contain name of libraries mentioned in requirements.txt file.
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .") # .remove("-e .") has been used here because already find_packages has bee used here which does the same work.
+
 
 
 setup(
