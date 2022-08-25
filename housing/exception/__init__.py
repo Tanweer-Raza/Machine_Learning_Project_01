6d7f01143f045bb2ff9c252f724fd40561f101f2
Tanweer Raza <tanatif@gmail.com>
2022-08-25 11:05:25 +0530

    updated

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -19,11 +19,12 @@ class HousingExcecption(Exception):
         file_name = exec_tb.tb_frame.f_code.co_filename
 
         error_message = f"""
-        Error occured in script: 
-        [ {file_name} ] at 
-        try block line number: [{try_block_line_number}] and exception block line number: [{exception_block_line_number}] 
-        error message: [{error_message}]
+        Error occured in script: [ {file_name} ] 
+        at try block line number: [ {try_block_line_number} ] 
+        and exception block line number: [ {exception_block_line_number} ] 
+        error message: [ {error_message} ]
         """
+
         return error_message
     def __str__(self):
         return self.error_message
